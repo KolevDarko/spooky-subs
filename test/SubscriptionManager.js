94f02e207cc5fa7b4d299ec464a832ea318e64e3
Darko Kolev <kolevdarko@gmail.com>
2023-02-07 19:40:13 +0100

    sub mgr tests

--- test/SubscriptionManager.js
+++ test/SubscriptionManager.js
@@ -4,6 +4,7 @@ const {
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+// const { BigNumber, ethers } = require("ethers");
 
 describe("SubscriptionManager", () => {
   async function deployContracts() {
@@ -21,6 +22,44 @@ describe("SubscriptionManager", () => {
       const tokenSupply = 1_000_000_000_000;
       expect(await token.totalSupply()).to.equal(tokenSupply);
       expect(await token.name()).to.equal("Test");
+    });
+    it("Can create subscription", async () => {
+      const { token, subManager } = await loadFixture(deployContracts);
+      const [owner, second, third] = await ethers.getSigners();
+      ONE_MONTH_IN_SECS = 30 * 24 * 60 * 60;
+      await subManager
+        .connect(second)
+        .createSubscription(
+          third.address,
+          ethers.utils.parseEther("5"),
+          ONE_MONTH_IN_SECS,
+          token.address,
+        );
+      const newSubscription = await subManager.subscriptions(0);
+      expect(newSubscription.payer).to.equal(second.address);
+      expect(newSubscription.token).to.equal(token.address);
+    });
+    it("should emit event", async () => {
+      const { token, subManager } = await loadFixture(deployContracts);
+      const [, second, third] = await ethers.getSigners();
+      const amount = ethers.utils.parseEther("5");
+      await expect(
+        subManager
+          .connect(second)
+          .createSubscription(
+            third.address,
+            amount,
+            ONE_MONTH_IN_SECS,
+            token.address,
+          ),
+      )
+        .to.emit(subManager, "SubscriptionCreated")
+        .withArgs(second.address, third.address, 0, [
+          second.address,
+          third.address,
+          amount,
+          token.address,
+        ]);
     });
   });
 });
